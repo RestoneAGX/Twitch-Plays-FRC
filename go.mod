a6c9743930c8a908b175d55c2e67637a995eed82
Red <redstonegameraa@gmail.com>
2022-07-29 17:26:25 -0400

    Mainly finished
    Using usb will be something to look into, otherwise this is mostly done, but not tested

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module FRC
 
 go 1.18
 
-require github.com/gempir/go-twitch-irc/v3 v3.1.0 // indirect
+require github.com/gempir/go-twitch-irc/v3 v3.2.0 // direct
--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
 
 	"github.com/gempir/go-twitch-irc/v3"
 )
 
 type comms struct {
-	command uint8
-	path    string
+	command, amount int8
+	path            string
 }
 
 // PATH is if string is != nil
@@ -27,23 +28,6 @@ func main() {
 	ConnectToRoboRio(bridge)
 }
 
-func ConnectToRoboRio(message chan comms) {
-	// Obtain connection
-	conn, err := net.Dial("tcp", "10.20.68.2:1735") // Change the port depending on which port is hosted on the RoboRio
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// Check for receiving Data && intepret
-	msg := <-message
-	if msg.path != "" {
-		//Send to Path Planner
-		conn.Write([]byte("Path:" + msg.path))
-	}
-
-	// Call commands accordingly
-}
-
 func ConnectToTwitch(bridge chan comms) {
 	client := twitch.NewClient("RedBot", "oauth:muc232tqsq9ethul0qxvqe5ev8b0xr")
 
@@ -52,26 +36,45 @@ func ConnectToTwitch(bridge chan comms) {
 
 		func() {
 			if message.Message[0] != '>' {
-				return
-			}
-
-			if message.Message[0] == '#' {
-				bridge <- comms{0, message.Message} //Send this data to RoboRio so it can actually be parsed
-				//Maybe parse this first and then send, depending on the efficiency and reqs
+				if message.Message[0] == '#' {
+					bridge <- comms{0, 0, message.Message} //Send this data to RoboRio so it can actually be parsed
+					//Maybe parse this first and then send, depending on the efficiency and reqs
+				}
 				return
 			}
 
 			inputs := strings.Split(message.Message[1:], " ")
+			if len(inputs) < 2 {
+				client.Say("redstoneagx", "Invalid Input: "+message.User.DisplayName) // Tell the user they submitted bad input
+				return
+			}
+			amount, err := strconv.Atoi(inputs[1])
+			if err != nil {
+				client.Say("redstoneagx", "Invalid Input: "+message.User.DisplayName) // Tell the user invalid input
+				return
+			}
+
+			var dir int8
 			switch inputs[0] {
 			case "u":
+				dir = 0
 			case "d":
-			case "r":
-			case "l":
+				dir = 1
+			case "ur":
+				dir = 2
+			case "ul":
+				dir = 3
+			case "dr":
+				dir = 4
+			case "dl":
+				dir = 5
 			}
+			bridge <- comms{dir, int8(amount), ""}
 		}()
 
 		if message.Message == "!shutdown" && message.User.DisplayName == "RedstoneAGX" {
 			client.Say("redstoneagx", "Chat Robot Input Deactivated...")
+			bridge <- comms{path: "ex"}
 			err := client.Disconnect()
 			if err != nil {
 				fmt.Println(err)
@@ -80,7 +83,7 @@ func ConnectToTwitch(bridge chan comms) {
 	})
 
 	client.OnConnect(func() {
-		client.Say("redstoneagx", "Chat Robot Input Active")
+		client.Say("redstoneagx", "Chat Robot Input Active...")
 	})
 
 	client.Join("redstoneagx")
@@ -88,5 +91,24 @@ func ConnectToTwitch(bridge chan comms) {
 	err := client.Connect()
 	if err != nil {
 		fmt.Println(err)
+	}
+}
+
+func ConnectToRoboRio(message chan comms) {
+	// Obtain connection
+	conn, err := net.Dial("tcp", "10.20.68.2:1735") // Change the port depending on which port is hosted on the RoboRio
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for {
+		msg := <-message // Check for receiving Data && intepret
+		if msg.path == "" {
+			conn.Write([]byte{byte(msg.command), byte(msg.amount)}) // Call commands accordingly
+		} else if msg.path == "ex" {
+			break
+		} else {
+			conn.Write([]byte("Path:" + msg.path)) //Send to Path Planner
+		}
 	}
 }
